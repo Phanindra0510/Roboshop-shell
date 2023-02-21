786cec0b96648fd58be0352d32c4dc265a38ce05
Phanindra Mahamkali <phanindra.mahamkali@gmail.com>
2023-02-22 02:32:03 +0530

    added log_file and single colour code file

--- frontend.sh
+++ frontend.sh
@@ -6,23 +6,23 @@ Print_head(){
   ech0 -e "\e[36$1\e[om"
 }
 Print_head "Installing nginx"
-yum install nginx -y &2>${log_file}
+yum install nginx -y &>>${log_file}
 
 Print_head "Removing old content nginx"
-rm -rf /usr/share/nginx/html/* &2>${log_file}
+rm -rf /usr/share/nginx/html/* &>>${log_file}
 
 Print_head "Downloading nginx"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &2>${log_file}
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
 Print_head "Extracting file nginx"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &2>${log_file}
+unzip /tmp/frontend.zip &>>${log_file}
 
 Print_head"Copying configs of nginx for roboshop"
-cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &2>${log_file}
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
 Print_head "Enabling nginx"
-systemctl enable nginx &2>${log_file}
+systemctl enable nginx &>>${log_file}
 
 Print_head "Starting nginx"
-systemctl restart nginx &2>${log_file}
+systemctl restart nginx &>>${log_file}

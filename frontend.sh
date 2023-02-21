9baa9562c7b1899c6476e3f6f8ee7d50e7e0024f
Phanindra Mahamkali <phanindra.mahamkali@gmail.com>
2023-02-21 21:33:21 +0530

    added code_dir for frontend config files

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,5 @@
+code_dir=$(pwd)
+
 echo -e "\e[36mInstalling nginx\e[0m"
 yum install nginx -y
 
@@ -12,7 +14,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 echo -e "\e[36mCopying configs of nginx for roboshop\e[0m"
-cp configs/nginx-roboshop.conf /etc/nginx/defaullt..d/roboshop..conf
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/defaullt..d/roboshop..conf
 
 echo -e "\e[36mEnabling nginx\e[0m"
 systemctl enable nginx

74f0d64026e4e81e3e8fbb5781fed9887423672d
Phanindra Mahamkali <phanindra.mahamkali@gmail.com>
2023-03-05 18:46:11 +0530

    adding status check for frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -2,22 +2,28 @@ source common.sh
 
 Print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
+status_check $?
 
 Print_head "Removing old content nginx"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
+status_check $?
 
 Print_head "Downloading nginx"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+status_check $?
 
 Print_head "Extracting file nginx"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
+status_check $?
 
 Print_head "Copying configs of nginx for roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+status_check $?
 
 Print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
+status_check $?
 
 Print_head "Starting nginx"
 systemctl restart nginx &>>${log_file}

0b3145b5484dd585dfa4f7f6a8d8ba1969fcbcce
Phanindra Mahamkali <phanindra.mahamkali@gmail.com>
2023-03-06 18:46:59 +0530

    adding condition to create directory in catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -9,13 +9,13 @@ yum install nodejs -y &>>${log_file}
 status_check $?
 
 Print_head "Adding user"
-if [$? != 0 ]; then
+if [$? -ne 0 ]; then
  useradd roboshop &>>${log_file}
 fi
 status_check $?
 
 Print_head "Creating Directory"
-if [ !-d /app ]; then
+if [ ! -d /app ]; then
  mkdir /app &>>${log_file}
 fi
 status_check $?

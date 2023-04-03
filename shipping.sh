38529d1fa982cf2019f3b3134fc4edff48f6fb6d
Phanindra Mahamkali <phanindra.mahamkali@gmail.com>
2023-04-03 20:02:45 +0530

    changes in common.sh and shipping.sh

--- shipping.sh
+++ shipping.sh
@@ -1,7 +1,7 @@
 source common.sh
 
 mysql_root_password=$1
-if [ -z "${mysql_root_password}" == "mysql" ]
+if [ -z "${mysql_root_password}" ]
  then
    echo -e "\e[31mMissing MySQL Root Password\e[0m"
    exit 1

073dfeff51863e608cd77f8ef88be888bfb23904
Phanindra Mahamkali <phanindra.mahamkali@gmail.com>
2023-04-03 18:00:11 +0530

    setup of mysql.sh and mysql.repo

--- mysql.sh
+++ mysql.sh
@@ -3,8 +3,8 @@ source common.sh
 mysql_root_password=$1
 if [ -z "${mysql_root_password}" ]
  then
-   echo-e "\e[31mMissing MySQL Root Password\e[0m"
-   exit1
+   echo -e "\e[31mMissing MySQL Root Password\e[0m"
+   exit 1
 fi
 
 Print_head "Disable MySQL 8 Version"

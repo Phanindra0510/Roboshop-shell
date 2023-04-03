022921a38574b051c08af9277d6e66f62ed8bc5d
Phanindra Mahamkali <phanindra.mahamkali@gmail.com>
2023-04-03 18:14:17 +0530

    setup of mysql.sh and mysql.repo

--- mysql.sh
+++ mysql.sh
@@ -28,5 +28,8 @@ systemctl start mysqld &>>${log_file}
 status_check $?
 
 Print_head "Set Root Password"
-mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
+echo show databases|mysql -uroot -p${mysql_root_password}&>>${log_file}
+if [ $? -ne 0 ]; then
+ mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
+fi
 status_check $?

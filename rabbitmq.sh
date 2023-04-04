c2896378f31db60e184ca68b46649667e9871162
Phanindra Mahamkali <phanindra.mahamkali@gmail.com>
2023-04-04 16:31:30 +0530

    setup of rabbitmq

---
+++ rabbitmq.sh
@@ -0,0 +1,40 @@
+source common.sh
+
+roboshop_app_password=$1
+if [ -z "${roboshop_app_password}" ]
+ then
+   echo -e "\e[31mMissing MySQL Root Password\e[0m"
+   exit 1
+fi
+
+Print_head "Setup of Erlang"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>${log_file}
+status_check $?
+
+Print_head "Install Erlang"
+yum install erlang -y &>>${log_file}
+status_check $?
+
+Print_head "Setup of RabbitMQ"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${log_file}
+status_check $?
+
+Print_head "Install RabbitMQ"
+yum install rabbitmq-server -y &>>${log_file}
+status_check $?
+
+Print_head "Enable RabbitMQ"
+systemctl enable rabbitmq-server &>>${log_file}
+status_check $?
+
+Print_head "Start RabbitMQ"
+systemctl start rabbitmq-server &>>${log_file}
+status_check $?
+
+Print_head "Add App User"
+rabbitmqctl add_user roboshop ${roboshop_app_password} &>>${log_file}
+status_check $?
+
+Print_head "Set App Permission"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${log_file}
+status_check $?
